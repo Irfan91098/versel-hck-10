4ff75be3ff55a7a0587aa6e1e9fb0b0d4b797886
Irfan91098 <147717878+Irfan91098@users.noreply.github.com>
2023-10-31 14:37:38 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+key = "hacktiv8mania2023"
+
+# public
+@app.get("/") # menentukan alamat/url
+def helloFunction(): # function yang memproses alamat/url tertentu
+    return {
+        "message": "hello world"
+    }
+
+# secret -> harus memasukan authentication
+@app.get("/secret")
+def helloFunction(api_keys: str = header(none)):
+    # check api_key dari header
+    if api_keys is none or api_keys != key:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+    
+    return {
+        "message": "secret message"
+    }
